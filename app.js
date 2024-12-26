50ca045ec1be4f1340c17ef7841741d2eb1e02c9
agarwaldeepesh060 <162901505+agarwaldeepesh060@users.noreply.github.com>
2024-12-26 20:08:56 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 document.getElementById('downloadbtn').addEventListener('click', function() {
     const link = document.createElement('a');
-    link.href = './downloads/deepesh agarwal resume.pdf';
+    link.href = 'deepesh agarwal resume.pdf';
     link.download = 'deepesh agarwal resume.pdf'; // Change this to the desired file name
     document.body.appendChild(link);
     link.click();
